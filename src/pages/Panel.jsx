8dc35a84ac4447ca48aebfaf68b6a6c53cf9bbb7
FerGarcia099 <fgarcia@ffacsa.com>
2026-08-17 22:03:24 -0600

    Corregir errores

--- src/pages/Panel.jsx
+++ src/pages/Panel.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom"
 import axios from "axios"
 import Sidebar from "../components/Sidebar"
 import "../App.css"
-import "./PanelAlumno.css"
+import "./Panel.css"
 import { API_URL } from "../config"
 
 
